40515b00c3cfc8d560c36dd024ad9ba275801a33
Amir2105 <Mr.pc.ir2@gmail.com>
2024-03-27 18:33:38 +0330

    Config Class RDbContext

--- RayaneGostar/Infra.Data/Context/RDbContetx.cs
+++ RayaneGostar/Infra.Data/Context/RDbContetx.cs
@@ -4,6 +4,9 @@ namespace RayaneGostar.Infra.Data.Context
 {
     public class RDbContetx:DbContext
     {
-        
+        public RDbContetx(DbContextOptions<RDbContetx>options):base(options)
+        {
+            
+        }
     }
 }
