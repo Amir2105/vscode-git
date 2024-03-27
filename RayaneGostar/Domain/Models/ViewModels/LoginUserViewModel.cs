ca048d74aba39656f9fe63b582b4db11cb707948
Amir2105 <Mr.pc.ir2@gmail.com>
2024-03-28 00:20:11 +0330

    انتقال اطلاعات مورد نیاز و enumمورد نیاز در LoginUserViewModel

--- RayaneGostar/Domain/Models/ViewModels/LoginUserViewModel.cs
+++ RayaneGostar/Domain/Models/ViewModels/LoginUserViewModel.cs
@@ -1,7 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RayaneGostar.Domain.Models.ViewModels
 {
     public class LoginUserViewModel
     {
         
+        [Display(Name = "شماره تلفن همراه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string? PhoneNumber { get; set; }
+
+        [Display(Name = "گذرواژه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string? Password { get; set; }
+        [Display(Name = "مرا به خاطر بسپار")]
+        public bool RememberMe { get; set; }
+
+    }
+    public enum LoginUserResult
+    {
+       NotFound,
+        NotActive,
+        Success,
+        IsBlocked
+ 
     }
 }
