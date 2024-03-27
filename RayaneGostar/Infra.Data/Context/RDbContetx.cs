f952638cfc51ed5075078cf504ea03e8c854be79
Amir2105 <Mr.pc.ir2@gmail.com>
2024-03-27 19:07:54 +0330

    افزودن DbSet به کلاس RDbContext

--- RayaneGostar/Infra.Data/Context/RDbContetx.cs
+++ RayaneGostar/Infra.Data/Context/RDbContetx.cs
@@ -1,12 +1,16 @@
 using Microsoft.EntityFrameworkCore;
+using RayaneGostar.Domain.Models.Account;
 
 namespace RayaneGostar.Infra.Data.Context
 {
-    public class RDbContetx:DbContext
+    public class RDbContetx : DbContext
     {
-        public RDbContetx(DbContextOptions<RDbContetx>options):base(options)
+        public RDbContetx(DbContextOptions<RDbContetx> options) : base(options)
         {
-            
+
         }
+        #region User
+        public DbSet<User> Users { get; set; }
+        #endregion
     }
 }
