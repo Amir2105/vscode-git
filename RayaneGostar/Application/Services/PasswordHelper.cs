42ed99d2c0bd3989fa1d7399995ff94a52fddf98
Amir2105 <Mr.pc.ir2@gmail.com>
2024-03-27 23:49:47 +0330

    ساخت کلاس PasswordHelper و انجام تنظیمات مربوطه

---
+++ RayaneGostar/Application/Services/PasswordHelper.cs
@@ -0,0 +1,22 @@
+using System.Security.Cryptography;
+using System.Text;
+using RayaneGostar.Application.Interfaces;
+
+namespace RayaneGostar.Application.Services
+{
+    public class PasswordHelper : IPasswordHelper
+    {
+        public string EncodePasswordMd5(string password)
+        {
+            Byte[] originalBytes;
+            Byte[] encodedBytes;
+            MD5 md5;
+            md5 = new MD5CryptoServiceProvider();
+            originalBytes = ASCIIEncoding.Default.GetBytes(pass);
+            encodedBytes = md5.ComputeHash(originalBytes);
+            return BitConverter.ToString(encodedBytes);
+
+        }
+
+    }
+}
