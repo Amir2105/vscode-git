f3dabccdd225fea0e37df1c4524f31ef0852417f
Amir2105 <Mr.pc.ir2@gmail.com>
2024-03-27 23:35:12 +0330

    تعریف RDbContext در UserRepository

--- RayaneGostar/Infra.Data/Repositories/UserRepository.cs
+++ RayaneGostar/Infra.Data/Repositories/UserRepository.cs
@@ -2,8 +2,21 @@ using RayaneGostar.Application.Interfaces;
 
 namespace RayaneGostar.Infra.Data.Repositories
 {
-    public class UserRepository:IUserReopsitory
+    public class UserRepository : IUserReopsitory
+
     {
-        
+        #region Constarctor
+        private readonly RDbContext _context;
+        public UserRepository(RDbContext context)
+        {
+            _context = context;
+        }
+
+        #endregion
+        public Task<bool> IsUserExitsPhoneNumbe(string phoneNumber)
+        {
+
+        }
+
     }
 }
