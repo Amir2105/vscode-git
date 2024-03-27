818f0bec2221f1944282b3ea4cab561e8da28716
Amir2105 <Mr.pc.ir2@gmail.com>
2024-03-27 19:27:42 +0330

    از user مواردی که میخواستیم اضافه کردیم و enum RegisterUserResult تعرف میکنیم

--- RayaneGostar/Domain/Models/ViewModels/RegisterUserViewModel.cs
+++ RayaneGostar/Domain/Models/ViewModels/RegisterUserViewModel.cs
@@ -1,7 +1,39 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RayaneGostar.Domain.Models.ViewModels
 {
     public class RegisterUserViewModel
     {
-        
+        [Display(Name = "نام")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string? FirstName { get; set; }
+
+        [Display(Name = "نام خانوادگی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string? LastName { get; set; }
+
+        [Display(Name = "شماره تلفن همراه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string? PhoneNumber { get; set; }
+
+        [Display(Name = "گذرواژه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string? Password { get; set; }
+        [Display(Name = "تکرار گذر واژه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        [Compare("Password", ErrorMessage = "کلمه عبور با هم مغایرت دارند")]
+        public string? ConfirmPassword { get; set; }
+
+    }
+    public enum RegisterUserResult
+    {
+    MobileExists,
+	Success  
+
     }
 }
