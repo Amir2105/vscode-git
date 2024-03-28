276219843fe2937839070909a6abe749c3a2fc5d
Amir2105 <Mr.pc.ir2@gmail.com>
2024-03-28 09:52:52 +0330

    افزودن Region sitefooter بهSiteViewComponet

--- RayaneGostar/ViewComponent/SiteViewComponent.cs
+++ RayaneGostar/ViewComponent/SiteViewComponent.cs
@@ -3,11 +3,21 @@ namespace RayaneGostar.Views.Shared.Component.ViewComponent
     public class SiteViewComponent
     {
         #region siteHeader
-        public class SiteHederViewComponent :ViewComponent
+        public class SiteHeaderViewComponent :ViewComponent
         {
             public async Task<IViewComponentResult>InvokeAsync()
             {
                return View("SiteHeader") ;
+            }
+        }
+            
+        #endregion
+            #region siteFooter
+        public class SiteFooterViewComponent :ViewComponent
+        {
+            public async Task<IViewComponentResult>InvokeAsync()
+            {
+               return View("SiteFooter") ;
             }
         }
             
