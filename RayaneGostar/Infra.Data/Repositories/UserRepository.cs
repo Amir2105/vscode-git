2faf6d7ce907a7eefd02cd167babb1c44fcfdbf2
Amir2105 <Mr.pc.ir2@gmail.com>
2024-03-28 11:06:19 +0330

    رفع خطا

--- RayaneGostar/Infra.Data/Repositories/UserRepository.cs
+++ RayaneGostar/Infra.Data/Repositories/UserRepository.cs
@@ -3,46 +3,53 @@ using RayaneGostar.Application.Interfaces;
 using RayaneGostar.Domain.Interfaces;
 using RayaneGostar.Domain.Models.Account;
 using RayaneGostar.Infra.Data.Context;
+using System;
+using System.Threading.Tasks;
 
 namespace RayaneGostar.Infra.Data.Repositories
 {
     public class UserRepository : IUserRepository
-
     {
-        #region Constarctor
+        #region Constructor
         private readonly RDbContext _context;
         public UserRepository(RDbContext context)
         {
             _context = context;
         }
-
         #endregion
+
         #region Account
-        public async Task<bool> IsUserExitsPhoneNumbe(string phoneNumber)
+        public async Task<bool> IsUserExistsPhoneNumber(string phoneNumber)
         {
             return await _context.Users.AsQueryable().AnyAsync(c => c.PhoneNumber == phoneNumber);
         }
-        public async Task CreateUser<User user>()
+
+        public async Task CreateUser(User user)
         {
             await _context.Users.AddAsync(user);
         }
 
-        public async Task SaveChange()
+        public async Task SaveChanges()
         {
             await _context.SaveChangesAsync();
-        }
-
-        public Task CreateUser(User user)
-        {
-            throw new NotImplementedException();
         }
 
         public async Task<User> GetUserByPhoneNumber(string phoneNumber)
         {
             return await _context.Users.AsQueryable().SingleOrDefaultAsync(c => c.PhoneNumber == phoneNumber);
         }
+
+        public Task<bool> IsUserExitsPhoneNumbe(string phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveChange()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        #endregion
     }
-    #endregion
-
-
 }
