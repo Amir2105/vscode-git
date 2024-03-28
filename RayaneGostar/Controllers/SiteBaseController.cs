4c5192e39950ca9194ba25a150aaef3454c31acd
Amir2105 <Mr.pc.ir2@gmail.com>
2024-03-28 12:01:05 +0330

    انجام تنظیمات SiteBaseController جهت Message

--- RayaneGostar/Controllers/SiteBaseController.cs
+++ RayaneGostar/Controllers/SiteBaseController.cs
@@ -4,6 +4,10 @@ namespace RayaneGostar.Controllers
 {
     public class SiteBaseController : Controller
     {
+        protected string ErrorMessage = "ErrorMessage";
+        protected string SuccessMessage = "SuccessMessage";
+        protected string InfoMessage = "InfoMessage";
+        protected string WarningMessage = "WarningMessage";
 
     }
 }
